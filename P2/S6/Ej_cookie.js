dab93b8ee53489a14932c3574196ea7bdb06ce53
raulruizs <r.ruizs.2018@alumnos.urjc.es>
2025-04-14 11:57:52 +0200

    Creo el primer ejemplo de cookies para leerlo usando node.js

---
+++ P2/S6/Ej_cookie.js
@@ -0,0 +1,9 @@
+//-- Leer la cookie
+const cookie = req.headers.cookie;
+
+if (cookie) {
+    //-- Si hay cookie definida...
+    //-- Hacer lo que toque...
+} else {  //-- NO hay ninguna cookie definida
+    //-- Hacer lo que toque
+}

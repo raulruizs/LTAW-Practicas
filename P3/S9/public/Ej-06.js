f71db7b58a7cde6e96b5d175926fabf03aa8f269
raulruizs <r.ruizs.2018@alumnos.urjc.es>
2025-05-10 21:13:06 +0200

    no me está funcionando

--- P3/S9/public/Ej-06.js
+++ P3/S9/public/Ej-06.js
@@ -6,25 +6,22 @@ const { WebSocketServer } = require('ws');
 const PUERTO = 8080;
 const app = express();
 
-// Sirve automáticamente archivos de la carpeta 'public'
+
 app.use(express.static('public'));
 
 const server = http.createServer(app);
 const wsServer = new WebSocketServer({ server });
 
+// WebSocket funcional
 wsServer.on('connection', socket => {
-  console.log("Cliente WebSocket conectado");
+  console.log("Cliente conectado por WebSocket");
 
-  socket.on('message', message => {
-    console.log("Mensaje recibido:", message.toString());
-    socket.send("Eco: " + message);
-  });
-
-  socket.on('close', () => {
-    console.log("Cliente desconectado");
+  socket.on('message', msg => {
+    console.log("Mensaje recibido:", msg.toString());
+    socket.send("Eco: " + msg);
   });
 });
 
 server.listen(PUERTO, () => {
-  console.log("Escuchando en puerto:", PUERTO);
+  console.log("Servidor escuchando en puerto:", PUERTO);
 });

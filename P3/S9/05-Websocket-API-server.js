cc73bb392884972bc9513c5715c98a776d352ac6
raulruizs <r.ruizs.2018@alumnos.urjc.es>
2025-05-12 18:43:51 +0200

    sigo llevando a cbo cambios

--- P3/S9/05-Websocket-API-server.js
+++ P3/S9/05-Websocket-API-server.js
@@ -1,10 +1,3 @@
-const express = require('express');
-const http = require('http');
-const { WebSocketServer } = require('ws');
-
-const PUERTO = 8080; 
-
-
 //-- Crear una aplicación web vacia
 const app = express();
 
@@ -12,14 +5,7 @@ const app = express();
 const server = http.Server(app);
 
 //-- Crear el servidor de websockets, asociado al servidor http
-const wsServer = new WebSocketServer({server});
-const path = require('path');
-
-// Sirve test.html cuando se accede a /
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'test.html'));
-});
-
+const wsServer = new WebSocketServer({httpServer: server});
 //-- Lanzar el servidor HTTP
 //-- ¡Que empiecen los juegos de los WebSockets!
 server.listen(PUERTO);

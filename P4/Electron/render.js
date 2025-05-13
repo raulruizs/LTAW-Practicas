d012c47619c4f824b2b43050496365861b1cb450
raulruizs <r.ruizs.2018@alumnos.urjc.es>
2025-05-13 13:22:24 +0200

    cambio el render para que compruebe que window.api existe

--- P4/Electron/render.js
+++ P4/Electron/render.js
@@ -1,15 +1,10 @@
-// renderer.js
-window.addEventListener('DOMContentLoaded', () => {
+if (window.api) {
   window.api.onVersionInfo((info) => {
     document.getElementById("v-node").textContent = info.node;
     document.getElementById("v-chrome").textContent = info.chrome;
     document.getElementById("v-electron").textContent = info.electron;
     document.getElementById("url-cliente").textContent = info.url;
     document.getElementById("url-cliente").href = info.url;
-    document.getElementById("btn-test").addEventListener("click", () => {
-        window.api.sendTestMessage("Este es un mensaje de prueba desde el servidor");
-});
-
   });
 
   window.api.onUserCount((count) => {
@@ -21,4 +16,8 @@ window.addEventListener('DOMContentLoaded', () => {
     p.innerText = msg;
     document.getElementById("display").appendChild(p);
   });
-});
+
+  document.getElementById("btn-test").addEventListener("click", () => {
+    window.api.sendTestMessage("📢 Este es un mensaje de prueba desde el servidor");
+  });
+}

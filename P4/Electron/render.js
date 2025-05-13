83db6dd8da087e502f05f5f4d308587fab88156e
raulruizs <r.ruizs.2018@alumnos.urjc.es>
2025-05-13 13:17:42 +0200

    quito emoticono de mensaje de prueba no le gusta al vscode

--- P4/Electron/render.js
+++ P4/Electron/render.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
     document.getElementById("url-cliente").textContent = info.url;
     document.getElementById("url-cliente").href = info.url;
     document.getElementById("btn-test").addEventListener("click", () => {
-        window.api.sendTestMessage("📢 Este es un mensaje de prueba desde el servidor");
+        window.api.sendTestMessage("Este es un mensaje de prueba desde el servidor");
 });
 
   });

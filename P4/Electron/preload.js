3aa0dc7f4ad3bd174071b9b68247a3aa84f3dee2
raulruizs <r.ruizs.2018@alumnos.urjc.es>
2025-05-13 13:10:54 +0200

    Actualizo e preload para añadir el mensaje de prueba

--- P4/Electron/preload.js
+++ P4/Electron/preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   onVersionInfo: (cb) => ipcRenderer.on('version-info', (_, info) => cb(info)),
   onUserCount: (cb) => ipcRenderer.on('usuarios', (_, count) => cb(count)),
-  onMessage: (cb) => ipcRenderer.on('msg_client', (_, msg) => cb(msg))
+  onMessage: (cb) => ipcRenderer.on('msg_client', (_, msg) => cb(msg)),
+  sendTestMessage: (msg) => ipcRenderer.invoke('mensaje-prueba', msg)
 });

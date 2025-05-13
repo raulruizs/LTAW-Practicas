ff2c5b712b039e0a551827961dab35c14f64237e
raulruizs <r.ruizs.2018@alumnos.urjc.es>
2025-05-13 12:29:37 +0200

    simplifico el render

--- P4/Electron/render.js
+++ P4/Electron/render.js
@@ -1,27 +1,20 @@
+// renderer.js
 window.addEventListener('DOMContentLoaded', () => {
-  const spanNode = document.getElementById("v-node");
-  const spanChrome = document.getElementById("v-chrome");
-  const spanElectron = document.getElementById("v-electron");
-  const spanUsuarios = document.getElementById("usuarios");
-  const aUrl = document.getElementById("url-cliente");
-  const mensajesDiv = document.getElementById("mensajes");
-
   window.api.onVersionInfo((info) => {
-    spanNode.textContent = info.node;
-    spanChrome.textContent = info.chrome;
-    spanElectron.textContent = info.electron;
-    spanUsuarios.textContent = "0";
-    aUrl.href = info.url;
-    aUrl.textContent = info.url;
+    document.getElementById("v-node").textContent = info.node;
+    document.getElementById("v-chrome").textContent = info.chrome;
+    document.getElementById("v-electron").textContent = info.electron;
+    document.getElementById("url-cliente").textContent = info.url;
+    document.getElementById("url-cliente").href = info.url;
   });
 
   window.api.onUserCount((count) => {
-    spanUsuarios.textContent = count;
+    document.getElementById("usuarios").textContent = count;
   });
 
   window.api.onMessage((msg) => {
-    const p = document.createElement('p');
+    const p = document.createElement("p");
     p.innerText = msg;
-    mensajesDiv.appendChild(p);
+    document.getElementById("display").appendChild(p);
   });
 });

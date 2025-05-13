19f146dc54eb5f3635c943e6a349df1e80fc6284
raulruizs <r.ruizs.2018@alumnos.urjc.es>
2025-05-13 12:25:19 +0200

    preload

--- P4/Electron/preload.js
+++ P4/Electron/preload.js
@@ -1,1 +1,8 @@
-    
+    // preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  onVersionInfo: (cb) => ipcRenderer.on('version-info', (_, info) => cb(info)),
+  onUserCount: (cb) => ipcRenderer.on('usuarios', (_, count) => cb(count)),
+  onMessage: (cb) => ipcRenderer.on('msg_client', (_, msg) => cb(msg)),
+});
